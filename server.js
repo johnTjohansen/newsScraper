d90f27f72118d8cdd7c42baf5d86b6d897b8efac
John Johansen <johnjohansen@Johns-MacBook-Pro.local>
2017-06-08 04:34:38 -0700

    Wed save

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
-var Comment = require("./models/Comment.js");
-var Article = require("./models/Article.js");
+var Comment = require("./models/comment.js");
+var Article = require("./models/article.js");
 
 var request = require("request");
-var cheerio = require("cheerio");
 
 mongoose.Promise = Promise;
 
@@ -20,6 +19,28 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static("public"));
 
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 mongoose.connect()
 var db = mongoose.connection;
+
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
+var routes = require("./controllers/newsController.js");
+app.use("/", routes);
+
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+});
 
